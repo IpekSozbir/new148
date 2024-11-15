588f3836a9de411441d248a1c4c597a8e604e550
IpekSozbir <ipekoren@gmail.com>
2024-11-15 11:44:38 +0300

    second commit

--- src/P01/P01_HelloWorld.java
+++ src/P01/P01_HelloWorld.java
@@ -3,6 +3,7 @@ package P01;
 public class P01_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello GitHub");
     }
 
 }
--- src/P02/P01.java
+++ src/P02/P01.java
@@ -1,9 +1,21 @@
 package P02;
 
+import java.util.Scanner;
+
 public class P01 {
     public static void main(String[] args) {
         int a = 128;
         int b = 17;
         System.out.println("c=" + (a+b));
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bir sayi giriniz");
+        int sayi = scan.nextInt();
+
+        if (sayi%2 == 0) {
+            System.out.println("girdiğiniz sayi bir cift sayidir");
+        } else {
+            System.out.println("girdiğiniz sayi bir cift sayi degildir");
+        }
     }
 }

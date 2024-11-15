1edc7ba53e09e1ecac4c26cf1beca43b907b4015
IpekSozbir <ipekoren@gmail.com>
2024-11-15 11:41:06 +0300

    first commit

---
+++ src/P01/P01_HelloWorld.java
@@ -0,0 +1,8 @@
+package P01;
+
+public class P01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/P02/P01.java
@@ -0,0 +1,9 @@
+package P02;
+
+public class P01 {
+    public static void main(String[] args) {
+        int a = 128;
+        int b = 17;
+        System.out.println("c=" + (a+b));
+    }
+}
